382c83aac52bec439a0cf5c723bad638872e7dec
sanghyeok-han <53252712+sanghyeok-han@users.noreply.github.com>
2023-06-22 17:53:46 +0900

    ver 230622

--- cpp/temp.cpp
+++ cpp/temp.cpp
@@ -59,7 +59,7 @@ int main()
       }
   
       for (int i=0;i<n;i++){
-          for (int j=0;j<n;i++){
+          for (int j=0;j<n;j++){
               if (i == j){
                   continue;
               }
